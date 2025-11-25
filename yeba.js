90d88bbadb54634e508df093d8ddd680bc0dffcc
Saidjon <225557527+Saidjon07@users.noreply.github.com>
2025-11-25 16:05:12 +0300

    Add files via upload

---
+++ yeba.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let tezlik = prompt("Tezlikni kiriting:");
+tezlik = Number(tezlik);
+
+if (tezlik <= 60) {
+    alert("Tabriklaymiz! Tezlik me'yorda.");
+} else {
+    alert(`Siz tezlikni ${tezlik - 60} km/h ga oshirdingiz! 360ming jarima`);
+}
